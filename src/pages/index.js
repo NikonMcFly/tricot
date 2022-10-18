7af5605d772711398af13b0f2b55a1ece9d99248
nikonmcfly <s@sosborne.co>
2022-10-18 16:19:58 -0500

    ran gastby build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import Header from "./header.js";
+// import Header from "./header.js";
 
 const pageStyles = {
   color: "#232129",
@@ -83,6 +83,33 @@ const hero = {
   backgroundColor: "#AA4A44",
   padding: "100px 0",
 }
+
+
+const Header = [
+  {
+    text: "Home",
+    url: "https://www.gatsbyjs.com/docs/tutorial/",
+    description:
+      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+    color: "#E95800",
+  },
+  {
+    text: "About",
+    url: "https://www.gatsbyjs.com/docs/how-to/",
+    description:
+      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
+    color: "#1099A8",
+  },
+  {
+    text: "Contact Us",
+    url: "https://www.gatsbyjs.com/docs/reference/",
+    description:
+      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
+    color: "#BC027F",
+  }
+]
+
+// export default Header;
 
 const links = [
   {
